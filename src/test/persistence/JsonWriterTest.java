39ca678b5803069ff4ac2f52d696ff245efb7c4b
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-03-05 23:42:42 -0800

    added more tests

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -75,6 +77,28 @@ class JsonWriterTest extends JsonTest {
             JsonReader reader = new JsonReader("./data/testWriterProfileTwo.json");
             Profile actual = reader.readProfile();
             checkProfile(expected, actual);
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterStocksTwo() {
+        try {
+            Stock s1 = new Stock("AAPL", BigDecimal.valueOf(20));
+            Stock s2 = new Stock("MSFT", BigDecimal.valueOf(120));
+            List<Stock> stocks = new ArrayList<>(Arrays.asList(s1, s2));
+            StockList expected = new StockList(stocks);
+            JsonWriter writer = new JsonWriter("./data/testWriterStocksTwo.json");
+            writer.open();
+            writer.write(expected);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterStocksTwo.json");
+            StockList actual = reader.readStockList();
+            for (int i = 0; i < actual.getSize(); i++) {
+                checkStock(expected.getStock(i), actual.getStock(i));
+            }
         } catch (IOException e) {
             fail("Exception should not have been thrown");
         }
